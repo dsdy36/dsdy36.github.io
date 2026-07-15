6c74fc7007541d0c5a5985b9330bfaf456130871
dsdy36 <1308192685@qq.com>
2026-07-15 08:55:38 +0800

    Delete FS-generator directory

--- FS-generator/references/q1_iMusic/Task2/RQ2_1_vD.py
+++
@@ -1,6 +0,0 @@
-def get_all_genres():
-    with sqlite3.connect(DB_FILE) as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT GenreId, Name FROM Genre ORDER BY Name ASC")
-        genres = [{'GenreId': row[0], 'Name': row[1]} for row in cursor.fetchall()]
-    return [{'GenreId': 0, 'Name': 'All'}] + genres
